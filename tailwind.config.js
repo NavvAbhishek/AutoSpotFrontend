8671f8b6fec336d112eb6584a0b8ffffd18da265
Navindu-Abhishek <abi2000navi@gmail.com>
2023-06-20 21:20:01 +0530

    finish header section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        xs: {'max': '639px'},
+      },
       colors: {
-        'main-color':'#018A8C',
-        'sec-color':'#FFE535',
-        'sec-color-h':'#F7D700',
+        "main-color": "#018A8C",
+        "sec-color": "#FFE535",
+        "sec-color-h": "#F7D700",
+      },
+      height: {
+        "70vh": "70vh",
+      },
+      width: {
+        "42rem": "42rem",
+        "30rem": "30rem",
       },
     },
   },
   plugins: [],
-}
+};
